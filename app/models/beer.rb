977079ebaf6f519273082a806c4f9800e8fc58ac
Koppari <robinstachon@gmail.com>
2017-01-29 08:22:26 +0200

    vk 2

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,13 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
+	has_many :ratings
+
+	def to_s
+		"#{name}" + " #{brewery.name}"
+	end
+
+	def average_rating
+		ratings.average(:score)
+	end
+
 end
