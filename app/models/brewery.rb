46c3a6c9b65ea120f133d25bb76d07f7c4f75ca5
Koppari <robinstachon@gmail.com>
2017-02-05 19:35:15 +0200

    vk3

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,7 +2,8 @@ class Brewery < ActiveRecord::Base
 	include RatingAverage
 
 	validates :name, presence: true
-			  :year, numericality: { greater_than_or_equal_to: 1042,
+	
+	validates :year, numericality: { greater_than_or_equal_to: 1042,
                                     less_than_or_equal_to: 2017,
                                     only_integer: true } 
 
