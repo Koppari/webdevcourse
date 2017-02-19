f194b30204974649954c728e01b669a9be29b2bc
Koppari <robinstachon@gmail.com>
2017-02-19 23:05:13 +0200

    vk5

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,6 +5,7 @@ class Beer < ActiveRecord::Base
   validates :style_id, presence: true
 
 	belongs_to :brewery
+	belongs_to :style
     has_many :ratings, dependent: :destroy
     has_many :raters, through: :ratings, source: :user
 
