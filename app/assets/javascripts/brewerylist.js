59467c3f3366a04e58cacc38f727548060f57bd1
Koppari <robinstachon@gmail.com>
2017-03-17 10:26:00 +0200

    vk7

--- app/assets/javascripts/brewerylist.js
+++ app/assets/javascripts/brewerylist.js
@@ -5,10 +5,13 @@ BREWERIES.show = function(){
 
     var table = $("#brewerytable");
 
-    $.each(BREWERIES.list, function (index, brewery) {
+    $.each(BREWERIES.list, function (index, beer) {
+        var active = beer['active'] ? 'yes' : 'no';
         table.append('<tr>'
-            +'<td>'+brewery['name']+'</td>'
-            +'<td>'+brewery['year']+'</td>'
+            +'<td>'+beer['name']+'</td>'
+            +'<td>'+beer['year']+'</td>'
+            +'<td>'+beer['beers']+'</td>'
+            +'<td>'+active+'</td>'
             +'</tr>');
     });
 };
@@ -21,31 +24,39 @@ BREWERIES.sort_by_name = function(){
 
 BREWERIES.sort_by_year = function(){
     BREWERIES.list.sort( function(a,b){
-        return a.year.toUpperCase().localeCompare(b.year.toUpperCase());
+        return a.year - b.year;
+    });
+};
+
+BREWERIES.sort_by_beers = function(){
+    BREWERIES.list.sort( function(a,b){
+        return b.beers - a.beers;
     });
 };
 
 $(document).ready(function () {
-    if ( $("#brewerytable").length>0 ) {
+  if ( $("#brewerytable").length>0 ) {    
+    $("#name").click(function (e) {
+        BREWERIES.sort_by_name();
+        BREWERIES.show();
+        e.preventDefault();
+    });
 
-        $("#name").click(function (e) {
-            BREWERIES.sort_by_name();
-            BREWERIES.show();
-            e.preventDefault();
-        });
+    $("#year").click(function (e) {
+        BREWERIES.sort_by_year(); 
+        BREWERIES.show()
+        e.preventDefault();
+    });
 
-        $("#year").click(function (e) {
-            BREWERIES.sort_by_year();
-            BREWERIES.show();
-            e.preventDefault();
-        });;
+    $("#number").click(function (e) {
+        BREWERIES.sort_by_beers(); 
+        BREWERIES.show()
+        e.preventDefault();
+    });
 
-
-        $.getJSON('breweries.json', function (breweries) {
-            BREWERIES.list = breweries;
-            BREWERIES.sort_by_name;
-            BREWERIES.show();
-        });
-
-    }
+    $.getJSON('breweries.json', function (breweries) {
+      BREWERIES.list = breweries
+      BREWERIES.show()
+    });
+  }
 });
